955f13f6fb73938505433b7cc041018334f7f8c5
Shravanth-MR <shravanthmr9602@gmail.com>
2023-06-06 01:27:50 +0530

    profile pic and username upload to firestore

--- Blogapp/app/src/main/java/com/example/blog_app/Home.kt
+++ Blogapp/app/src/main/java/com/example/blog_app/Home.kt
@@ -1,11 +1,34 @@
 package com.example.blog_app
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class Home : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var logoutButton:Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        auth = Firebase.auth
+        logoutButton = findViewById(R.id.logout_button)
+
+        logoutButton.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+            //hello
+        }
+
     }
 }
