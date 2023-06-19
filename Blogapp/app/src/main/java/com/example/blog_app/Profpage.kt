05a3450ed5a55f9d0e12b8a85f911f8395ba1bb9
Shravanth-MR <shravanthmr9602@gmail.com>
2023-06-20 02:09:41 +0530

    profile page blogs display

--- Blogapp/app/src/main/java/com/example/blog_app/Profpage.kt
+++ Blogapp/app/src/main/java/com/example/blog_app/Profpage.kt
@@ -1,26 +1,27 @@
 package com.example.blog_app
 
-import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.google.firebase.auth.FirebaseAuth
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.blog_app.Blog
+import com.example.blog_app.R
+//import com.google.firebase.Timestamp
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
-//import java.security.Timestamp
-import java.text.SimpleDateFormat
+//import java.text.SimpleDateFormat
 import java.util.Locale
 import com.google.firebase.Timestamp
+import com.google.firebase.firestore.Query
+import java.util.Date
+import java.text.SimpleDateFormat
 
 
 class Profpage : AppCompatActivity() {
@@ -28,6 +29,7 @@ class Profpage : AppCompatActivity() {
     private lateinit var usernameTextView: TextView
     private lateinit var titleTextView: TextView
     private lateinit var blogPostsLayout: LinearLayout
+    private lateinit var userId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,12 +41,16 @@ class Profpage : AppCompatActivity() {
         blogPostsLayout = findViewById(R.id.blogPostsLayout)
         val leftArrowButton = findViewById<ImageButton>(R.id.leftArrowButton)
 
-
         leftArrowButton.setOnClickListener {
             finish()
         }
 
-        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        userId = Firebase.auth.currentUser?.uid ?: ""
+        if (userId.isNotEmpty()) {
+            fetchUserBlogs()
+        }
+
+        val userId = Firebase.auth.currentUser?.uid
         if (userId != null) {
             val db = FirebaseFirestore.getInstance()
             val userRef = db.collection("userProfile").document(userId)
@@ -56,8 +62,11 @@ class Profpage : AppCompatActivity() {
                         val title = documentSnapshot.getString("title")
                         val imageUrl = documentSnapshot.getString("image_url")
 
-                        usernameTextView.text = username
-                        titleTextView.text = title
+                        usernameTextView.text = username ?: ""
+                        titleTextView.text = title ?: ""
+
+//                        usernameTextView.text = username
+//                        titleTextView.text = title
 
                         // Load profile image using Picasso or any other image loading library
                         if (!imageUrl.isNullOrEmpty()) {
@@ -66,89 +75,79 @@ class Profpage : AppCompatActivity() {
                     }
                 }
                 .addOnFailureListener { error ->
-                // Handle the error gracefully
-                    showToast("Failed to retrieve user profile: ${error.message}", Toast.LENGTH_SHORT)
-
-                    Log.e("Profpage", "Failed to retrieve user profile", error)
-            }
-
-            val blogsRef = db.collection("blogs").whereEqualTo("userId", userId)
-
-            blogsRef.get()
-                .addOnSuccessListener { querySnapshot ->
-                    for (document in querySnapshot) {
-                        val blogId = document.id
-                        val blogData = document.data
-                        val title = blogData["title"] as String
-                        val description = blogData["description"] as String
-                        val timestamp = blogData["timestamp"] as? Timestamp
-                        val userId = blogData["userId"] as? String  // Fetch the userId from the blog data
-                        val imageUrlblog = blogData["image_url"] as? String  // Fetch the image URL from the blog data
-
-                        // Create a blog post view
-                        val blogPostView = LayoutInflater.from(this).inflate(R.layout.item_blog_post, null)
-
-                        // Set the profile image and username
-                        val profileImageView = blogPostView.findViewById<ImageView>(R.id.profileImageView)
-                        val usernameTextView = blogPostView.findViewById<TextView>(R.id.usernameTextView)
-
-                        // Load the user profile image using Picasso or any other image loading library
-                        val imageUrl = blogData["image_url"] as? String
-                        if (!imageUrl.isNullOrEmpty()) {
-                            Picasso.get().load(imageUrl).into(profileImageView)
-                        }
-                        val username = blogData["username"] as? String
-                        usernameTextView.text = username
-
-                        // Set the blog post details
-                        val titleTextView = blogPostView.findViewById<TextView>(R.id.titleTextView)
-                        val descriptionTextView = blogPostView.findViewById<TextView>(R.id.descriptionTextView)
-
-                        titleTextView.text = title
-                        descriptionTextView.text = description
-
-                        // Set the blog post date
-                        val dateTextView = blogPostView.findViewById<TextView>(R.id.dateTextView)
-                        timestamp?.let {
-                            val date = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(it.toDate())
-                            dateTextView.text = date
-                        }
-
-                        // Set the background color of the profile image circle
-                        val circleBackground = profileImageView.background as GradientDrawable
-                        val color = ContextCompat.getColor(this, R.color.profile_image_circle_background)
-                        circleBackground.setColor(color)
-
-
-
-                        // Set the click listeners for edit and delete buttons
-                        val editButton = blogPostView.findViewById<Button>(R.id.editButton)
-                        val deleteButton = blogPostView.findViewById<Button>(R.id.deleteButton)
-
-                        editButton.setOnClickListener {
-                            // Handle edit button click
-                            // Implement your logic here
-                        }
-
-                        deleteButton.setOnClickListener {
-                            // Handle delete button click
-                            // Implement your logic here
-                        }
-
-                        // Add the blog post view to the layout
-                        blogPostsLayout.addView(blogPostView)
-                    }
-                }
-                .addOnFailureListener { error ->
                     // Handle the error gracefully
-                    showToast("Failed to retrieve blog posts: ${error.message}")
-                    Log.e("Profpage", "Failed to retrieve blog posts", error)
+                    showToast("Failed to retrieve user profile: ${error.message}", Toast.LENGTH_SHORT)
+                    Log.e("Profpage", "Failed to retrieve user profile", error)
                 }
         }
     }
 
-    private fun showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    private fun showToast(message: String, duration: Int) {
         Toast.makeText(this, message, duration).show()
     }
+
+
+    private fun fetchUserBlogs() {
+        val db = FirebaseFirestore.getInstance()
+        val blogsRef = db.collection("blogs")
+
+        blogsRef.whereEqualTo("userId", userId)
+            .orderBy("timestamp", Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                for (document in querySnapshot) {
+                    val blog = document.toObject(Blog::class.java)
+                    val timestamp = blog.timestamp
+                    val blogItemView = LayoutInflater.from(this).inflate(R.layout.item_blog_post, blogPostsLayout, false)
+
+                    // Set blog details
+                    blogItemView.findViewById<TextView>(R.id.titleTextView).text = blog.title
+                    blogItemView.findViewById<TextView>(R.id.contentTextView).text = blog.description
+                    blogItemView.findViewById<TextView>(R.id.timestampTextView).text = formatDate(timestamp)
+
+                    // Load and display the blog image using Glide
+                    val imageView = blogItemView.findViewById<ImageView>(R.id.imageView1)
+                    if (blog.image?.isNotEmpty() == true) {
+                        Glide.with(this)
+                            .load(blog.image)
+                            .apply(RequestOptions().placeholder(R.drawable.placeholder_image)) // Placeholder image while loading
+                            .into(imageView)
+                    } else {
+                        // No image available, so hide the ImageView
+                        imageView.visibility = View.GONE
+                    }
+
+
+                    // Set click listeners for update and delete buttons
+                    val updateButton = blogItemView.findViewById<Button>(R.id.updateButton)
+                    val deleteButton = blogItemView.findViewById<Button>(R.id.deleteButton)
+
+                    updateButton.setOnClickListener {
+                        // Handle update blog button click
+                        // Implement the logic to open the update blog screen for the selected blog
+                    }
+
+                    deleteButton.setOnClickListener {
+                        // Handle delete blog button click
+                        // Implement the logic to delete the selected blog
+                    }
+
+                    // Add the blog item view to the blogPostsLayout
+                    blogPostsLayout.addView(blogItemView)
+                }
+            }
+            .addOnFailureListener { error ->
+                showToast("Failed to retrieve user blogs: ${error.message}", Toast.LENGTH_SHORT)
+                Log.e("Profpage", "Failed to retrieve user blogs", error)
+            }
+    }
+
+    private fun formatDate(timestamp: Date?): String {
+        return timestamp?.let {
+            val dateFormat = SimpleDateFormat("MMM dd, yyyy - hh:mm a", Locale.US)
+            dateFormat.format(it)
+        } ?: ""
+    }
+
 
 }
